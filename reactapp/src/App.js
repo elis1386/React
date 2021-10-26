caf7176379b44a48eafe1187ea0b6f2d8b9db024
Elizaveta <elis1386@gmail.com>
2021-10-26 13:19:20 +0300

    deleted <p>

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -13,9 +13,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Message message={text}  onMessageClick={handleClick}/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
